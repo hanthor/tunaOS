19e7b00f576c7610ea2466b69f4a08aac165d9c1
James Reilly <jreilly1821@gmail.com>
2025-06-02 21:53:51 +0530

    remove Docker installation commands from package script (#97)
    * remove Docker installation commands from package script
    
    * .

--- build_scripts/overrides/dx/00-packages.sh
+++ build_scripts/overrides/dx/00-packages.sh
@@ -10,15 +10,6 @@ dnf config-manager --add-repo "https://packages.microsoft.com/yumrepos/vscode"
 dnf config-manager --set-disabled packages.microsoft.com_yumrepos_vscode
 dnf -y --enablerepo packages.microsoft.com_yumrepos_vscode --nogpgcheck  install code
 
-dnf config-manager --add-repo "https://download.docker.com/linux/centos/docker-ce.repo"
-dnf config-manager --set-disabled docker-ce-stable
-dnf -y --enablerepo docker-ce-stable install \
-	docker-ce \
-	docker-ce-cli \
-	containerd.io \
-	docker-buildx-plugin \
-	docker-compose-plugin
-
 dnf -y --enablerepo copr:copr.fedorainfracloud.org:ublue-os:packages install \
   libvirt \
   libvirt-daemon-kvm \
