1b5baf5a73a698ba012629d35ef35e9d6ee9fbfe
James Reilly <jreilly1821@gmail.com>
2025-06-22 16:30:00 -0400

    Update 00-packages.sh
    -y

--- build_scripts/overrides/dx/00-packages.sh
+++ build_scripts/overrides/dx/00-packages.sh
@@ -9,7 +9,7 @@ dnf install -y \
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf check-update
-sudo dnf install code
+sudo dnf install -y code
 
 dnf config-manager addrepo --from-repofile="https://download.docker.com/linux/fedora/docker-ce.repo"
 dnf config-manager --set-disabled docker-ce-stable
