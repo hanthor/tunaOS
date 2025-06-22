a0a4f5306a6d6d5eba0ef3bb648e4ac936dc857d
James Reilly <jreilly1821@gmail.com>
2025-06-22 17:30:45 +0530

    fix: update hostname references from 'bluefin' to 'albacore' in image info scripts

--- build_scripts/overrides/gdx/90-image-info.sh
+++ build_scripts/overrides/gdx/90-image-info.sh
@@ -3,8 +3,8 @@
 set -xeuo pipefail
 
 FLAVOR="gdx"
-IMAGE_NAME="bluefin-${FLAVOR}"
-IMAGE_REF="ostree-image-signed:docker://ghcr.io/ublue-os/bluefin-${FLAVOR}"
+IMAGE_NAME="albacore-${FLAVOR}"
+IMAGE_REF="ostree-image-signed:docker://ghcr.io/ublue-os/albacore-${FLAVOR}"
 export FLAVOR
 export IMAGE_NAME
 export IMAGE_REF
